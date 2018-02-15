7fe41eca2b6bf001bf3e956b408dd40fec6c2c28
Jeniffer <jennim24@hotmail.com>
2018-02-15 11:21:25 -0600

    ventana  para alojamiento

--- Pagina Principal/Pagina Principal/Program.cs
+++ Pagina Principal/Pagina Principal/Program.cs
@@ -16,7 +16,7 @@ namespace Pagina_Principal
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form2()); //Form1()
         }
     }
 }
