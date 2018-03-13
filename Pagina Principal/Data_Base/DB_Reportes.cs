e38fb48c8b83f904c4665c7e8887d6c26af74aac
Jeniffer <jennim24@hotmail.com>
2018-03-13 13:30:38 -0600

    sexto reporte hecho

--- Pagina Principal/Data_Base/DB_Reportes.cs
+++ Pagina Principal/Data_Base/DB_Reportes.cs
@@ -92,5 +92,30 @@ namespace Data_Base
                 return tercerReporte;
             }
         }
+
+        public ArrayList sextoreporte()
+        {
+            ArrayList sextoreporte = new ArrayList();
+            connection = conexion1.Conexion();
+            connection.Open();
+            NpgsqlCommand cmd = new NpgsqlCommand("SELECT v.marca,COUNT(v.marca) AS cantidad FROM reservas as c JOIN vehiculos AS v ON c.vehiculo = Cast(v.codigo as integer) WHERE c.tipo_accion = true GROUP BY v.marca ORDER BY cantidad", connection);
+            NpgsqlDataReader reader = cmd.ExecuteReader();
+            try
+            {
+                while (reader.Read())
+                {
+                    sextoreporte.Add(reader.GetString(0));
+                    sextoreporte.Add(reader.GetInt32(1));
+                }
+                connection.Close();
+                return sextoreporte;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error no se pudo conectar a la base de datos. " + ex, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                connection.Close();
+                return sextoreporte;
+            }
+        }
     }
 }
--- Pagina Principal/Pagina Principal/Reportes_Admin.cs
+++ Pagina Principal/Pagina Principal/Reportes_Admin.cs
@@ -141,10 +141,7 @@ namespace Pagina_Principal
 
         ///////////////////////////////////////////////////////////////////////////////////////
 
-        public void Reporte3()
-        {
 
-        }
 
         private void btnSalir_Click(object sender, EventArgs e)
         {
@@ -155,18 +152,22 @@ namespace Pagina_Principal
 
         private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            if (tabControl1.SelectedIndex == 0)
+            {
+                Reporte1();
+            }
+
             if (tabControl1.SelectedIndex == 1)
             {
                 Reporte2();
             }
-
-            if (tabControl1.SelectedIndex == 2)
-            {
-                Reporte3();
-            }
             if (tabControl1.SelectedIndex == 2)
             {
                 tercerReporte();
+            }
+            if (tabControl1.SelectedIndex == 5)
+            {
+                sextoReporte();
             }
             
         }   
@@ -186,6 +187,23 @@ namespace Pagina_Principal
                 i++;
             }
             Grafico3.Series["Porcentaje de visitas por pais"].IsValueShownAsLabel = true;
+        }
+
+        public void sextoReporte()
+        {
+            DB_Reportes repor = new DB_Reportes();
+            ArrayList reporte6 = repor.sextoreporte();
+            this.Grafico6.Series.Clear();
+            this.Grafico6.Titles.Clear();
+            this.Grafico6.Titles.Add("Marcas de los vehículos más rentados");
+            Series Grafico = this.Grafico6.Series.Add("Marcas de los vehículos más rentados");
+            Grafico.ChartType = SeriesChartType.Pie;
+            for (int i = 0; i < reporte6.Count; i++)
+            {
+                Grafico.Points.AddXY(reporte6[i], reporte6[i + 1]);
+                i++;
+            }
+            Grafico6.Series["Marcas de los vehículos más rentados"].IsValueShownAsLabel = true;
         }
     }
 }
